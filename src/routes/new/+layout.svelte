6549a84efb82ddc9a1aeb74851e957124540e9a3
LPe7 <lapingenieur@gmail.com>
2025-08-03 22:21:01 +0200

    divergent changes were made here

--- src/routes/new/+layout.svelte
+++ src/routes/new/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-  import Navigation from "./Navigation.svelte";
+  import NavigationLarge from "./NavigationLarge.svelte";
+  import NavigationSmall from "./NavigationSmall.svelte";
   import ScrollUp from "./ScrollUp.svelte";
+  
+  let page_list_new = {
+    "self": {
+      name: "A propos",
+      hashes: {}},
+    "insa": "INSA",
+  }
+  let page_list = {
+    "self": "A propos",
+    "insa": "INSA",
+  }
+
 </script>
 
-<Navigation prefix="new" />
+<NavigationLarge minScreenSize="1000px" prefix="new" {page_list}/>
+<NavigationSmall maxScreenSize="1000px" prefix="new" {page_list}/>
 <ScrollUp />
 
 <main>
